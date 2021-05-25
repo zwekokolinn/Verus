ee1f5bb179cad05821fefaa5567486ce8e4916dc
epenx <84466662+epenx@users.noreply.github.com>
2021-05-26 01:47:35 +0700

    Update verus.sh

--- verus.sh
+++ verus.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz && tar xf hellminer_cpu_linux.tar.gz && ./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u RDGgnUxe7mCdxSNQetQ5kE6L48GN11ApNi.Encil -p x --cpu 46
+wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz && tar xf hellminer_cpu_linux.tar.gz && ./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u RDGgnUxe7mCdxSNQetQ5kE6L48GN11ApNi.Encil -p x --cpu 1
