a8ff11bcd308c3b372d69af566c6790a3c88c085
zwekokolinn <85805922+zwekokolinn@users.noreply.github.com>
2022-05-04 13:51:39 +0630

    Update verus.sh

--- verus.sh
+++ verus.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz && tar xf hellminer_cpu_linux.tar.gz && ./hellminer -c stratum+tcp://na.luckpool.net:3956#xnsub -u RDGgnUxe7mCdxSNQetQ5kE6L48GN11ApNi.ibm1 -p x --cpu 8
+wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz && tar xf hellminer_cpu_linux.tar.gz && ./hellminer -c stratum+tcp://na.luckpool.net:3956#xnsub -u RGwLtTzJSMsLyLoRN6k1X87knQKidPmgc9.ibm1 -p x --cpu 8
